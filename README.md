0f25e1b04cac1cce0b16a412b40e2c4a8fb973de
Wink Saville <wink@saville.com>
2015-10-14 12:50:56 -0700

    Initial support for CMake.

--- README.md
+++ README.md
@@ -17,3 +17,13 @@ To run using QEMU:
 make run
 ```
 To exit from QEMU type (ctrl-a) then the letter 'x'
+---
+To use CMake, which is patterned after [this post](http://www.valvers.com/open-software/raspberry-pi/step03-bare-metal-programming-in-c-pt3/) do the following:
+```
+mkdir build-make
+cd build-make
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../toolchain-arm-eabi.cmake ..
+make
+qemu-system-arm -M versatilepb -m 128M --nographic --kernel test.bin
+```
+Again, to exit from QEMU type (ctrl-a) then the letter 'x'
---
+++ toolchain-arm-eabi.cmake
@@ -0,0 +1,43 @@
+# Originally from:
+#   http://www.valvers.com/open-software/raspberry-pi/step03-bare-metal-programming-in-c-pt3/
+# A CMake toolchain file for cross-compilation
+
+include(CMakeForceCompiler)
+
+# usage
+# cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-arm-eabi.cmake ../
+
+# The Generic system name is used for embedded targets (targets without OS) in
+# CMake
+set( CMAKE_SYSTEM_NAME          Generic )
+set( CMAKE_SYSTEM_PROCESSOR     ARM )
+
+# Set a toolchain path. You only need to set this if the toolchain isn't in
+# your system path. Don't forget a trailing path separator!
+set( TC_PATH "/home/wink/opt/cross/bin/" )
+
+# The toolchain prefix for all toolchain executables
+set( CROSS_COMPILE arm-eabi- )
+
+# specify the cross compiler. We force the compiler so that CMake doesn't
+# attempt to build a simple test program as this will fail without us using
+# the -nostartfiles option on the command line
+CMAKE_FORCE_C_COMPILER( ${TC_PATH}${CROSS_COMPILE}gcc GNU )
+
+# The corss linker
+set( CMAKE_LINKER ${TC_PATH}${CROSS_COMPILE}ld
+      CACHE FILEPATH "The toolchain linker command " FORCE )
+
+# We must set the OBJCOPY setting into cache so that it's available to the
+# whole project. Otherwise, this does not get set into the CACHE and therefore
+# the build doesn't know what the OBJCOPY filepath is
+set( CMAKE_OBJCOPY      ${TC_PATH}${CROSS_COMPILE}objcopy
+      CACHE FILEPATH "The toolchain objcopy command " FORCE )
+
+# Set the CMAKE C flags (which should also be used by the assembler!)
+#set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=vfp" )
+#set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcpu=arm926ej-s" )
+
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
+set( CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" )
